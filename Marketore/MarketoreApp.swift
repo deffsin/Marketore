068920dfe4d4328d29c2f86c326f93270922bbbb
deffsin <deffsinco@gmail.com>
2024-01-26 10:24:35 +0200

    Initial Commit

---
+++ Marketore/MarketoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketoreApp.swift
+//  Marketore
+//
+//  Created by Denis Sinitsa on 26.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MarketoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
