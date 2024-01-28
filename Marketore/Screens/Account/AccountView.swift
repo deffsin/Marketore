7d5c0a82a46cd5da8d9e7740a85d9352e0fcdfed
deffsin <deffsinco@gmail.com>
2024-01-28 23:26:53 +0200

    Some part of the UI has been added to the Account

--- Marketore/Screens/Account/AccountView.swift
+++ Marketore/Screens/Account/AccountView.swift
@@ -8,11 +8,100 @@
 import SwiftUI
 
 struct AccountView: View {
+    @Environment(\.colorScheme) var colorScheme
     @ObservedObject var viewModel: AccountViewModel
     
+    @State private var scrollOffset: CGFloat = 0
+    @State private var isStarFilled: Bool = false
+    
     var body: some View {
-        Text("AccountView")
+        NavigationStack {
+            buildMainContent()
+        }
     }
+    
+    @ViewBuilder
+    func buildMainContent() -> some View {
+        ZStack {
+            ScrollView {
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("AccountView")
+                    }
+                    Spacer()
+                }
+                .background(GeometryReader {
+                    Color.clear.preference(key: ViewOffsetKey.self, value: $0.frame(in: .global).minY)
+                })
+                .padding(.horizontal, 15)
+            }
+            .padding(.top, 50)
+            .refreshable {
+                // refresh....
+            }
+            .onPreferenceChange(ViewOffsetKey.self) { offset in
+                withAnimation {
+                    self.scrollOffset = offset
+                }
+            }
+            .overlay {
+                customNavBar(offset: scrollOffset)
+            }
+        }
+    }
+    
+    func customNavBar(offset: CGFloat) -> some View {
+        let maxOffset: CGFloat = 20.0
+        let limitedOffset = min(maxOffset, abs(offset)) * (offset < 0 ? -2 : 2)
+        
+        let isDarkMode = colorScheme == .dark
+        let isNegativeOffset = offset < 0
+        
+        return ZStack {
+            Group {
+                if isDarkMode {
+                    isNegativeOffset ? Color(red: 0.10, green: 0.10, blue: 0.10) : Color(red: 0.06, green: 0.06, blue: 0.06)
+                } else {
+                    isNegativeOffset ? Color(red: 0.90, green: 0.90, blue: 0.90) : Color.white
+                }
+            }
+            .ignoresSafeArea(.all)
+            
+            HStack {
+                Button(action: { }) {
+                    Image(systemName: "bookmark")
+                        .resizable()
+                        .foregroundColor(.white)
+                        .padding(9)
+                        .background(.clear)
+                        .frame(width: 39, height: 48)
+                        .offset(x: -limitedOffset * 0.1)
+                }
+                Spacer()
+                
+                Text("Account")
+                    
+                Spacer()
+                
+                Button(action: { }) {
+                    Image(systemName: "plus")
+                        .resizable()
+                        .foregroundColor(.white)
+                        .padding(9)
+                        .background(Circle().fill(Color(appColor: .purpleColor)))
+                        .frame(width: 39, height: 39)
+                        .offset(x: limitedOffset * 0.1)
+                }
+            }
+            .font(.title)
+            .padding(.horizontal, 15)
+            .padding(.vertical, 7)
+        }
+        .frame(height: 35)
+        .frame(maxHeight: .infinity, alignment: .top)
+    }
+
 }
 
 #Preview {
