b0e237bcf913537d874639151926c6cfcc8e2df9
deffsin <deffsinco@gmail.com>
2024-02-01 15:57:52 +0200

    Implemented custom nav bar

--- Marketore/Screens/Account/Add product/AddProductView.swift
+++ Marketore/Screens/Account/Add product/AddProductView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct AddProductView: View {
+    @Environment(\.colorScheme) var colorScheme
+    @Environment(\.dismiss) var dismiss
     @ObservedObject var viewModel: AddProductViewModel
     
     var body: some View {
         NavigationStack {
             buildMainContent()
         }
+        .navigationBarBackButtonHidden(true)
     }
     
     @ViewBuilder
@@ -21,7 +24,7 @@ struct AddProductView: View {
         ScrollView(.vertical) {
             TagLayout(alignment: .center, spacing: 10) {
                 ForEach(viewModel.tags, id: \.self) { tag in
-                    tagView(tag, viewModel.selectedTag == tag ? .purple : .gray)
+                    tagView(tag, viewModel.selectedTag == tag ? Color(appColor: .purpleColor) : .gray)
                         .onTapGesture {
                             withAnimation(.easeInOut) {
                                 if viewModel.selectedTag == tag {
@@ -30,10 +33,46 @@ struct AddProductView: View {
                                     viewModel.selectedTag = tag
                                 }
                             }
-                         }
+                        }
                 }
             }
         }
+        .padding(.top, 50)
+        .overlay {
+            customNavBar()
+        }
+    }
+    func customNavBar() -> some View {
+        return ZStack {
+            Group {
+                Color(red: 0.06, green: 0.06, blue: 0.06)
+            }
+            .ignoresSafeArea(.all)
+            
+            HStack {
+                HStack {
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        HStack(spacing: 5) {
+                            Image(systemName: "chevron.left")
+                            Text("Back")
+                            Spacer()
+                        }
+                    }
+                }
+                .font(.system(size: 17))
+                .foregroundStyle(.white)
+                .padding(.horizontal, 1)
+                
+                Spacer()
+            }
+            .font(.title)
+            .padding(.horizontal, 15)
+            .padding(.vertical, 10)
+        }
+        .frame(height: 35)
+        .frame(maxHeight: .infinity, alignment: .top)
     }
     
     func tagView(_ tag: String, _ color: Color) -> some View {
